a82c2726027f1f90c040769fe480ca72eeba0fba
Lepoldo Caballero <git@pitakill.net>
2019-05-28 13:44:46 -0500

    Setup Config outside the package

---
+++ config.go
@@ -0,0 +1,15 @@
+package aws_resources
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+)
+
+var cfg aws.Config
+
+func SetConfig(localConf aws.Config) {
+	cfg = localConf
+}
+
+func Config() aws.Config {
+	return cfg
+}
